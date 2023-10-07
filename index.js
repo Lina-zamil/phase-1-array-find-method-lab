48f2c133af06a841a60377de669d17355c66b1ac
lina abumahfood <lina.abumahfood@gmail.com>
2023-10-07 16:34:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+const record = [
+    { year: "2018", result: "L" },
+    { year: "2017", result: "L" },
+    { year: "2016", result: "N/A" },
+    { year: "2015", result: "W" }
+    //...
+]
+function superbowlWin(arr) {
+    const myResult = arr.find(el => el.result === "W")
+    return myResult ? myResult.year : undefined
+};
+console.log(superbowlWin(record));
+
+
+
